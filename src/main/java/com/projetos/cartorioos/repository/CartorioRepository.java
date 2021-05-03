d9ee343a48266b224ab3a625d87de5a63c89d508
Rodrigo <rodrigoassis1997@gmail.com>
2021-05-02 21:49:40 -0300

    para utilizar funcionaldades do CRUD/JPA

---
+++ src/main/java/com/projetos/cartorioos/repository/CartorioRepository.java
@@ -0,0 +1,9 @@
+package com.projetos.cartorioos.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.projetos.cartorioos.entity.Cartorio;
+
+public interface CartorioRepository extends CrudRepository<Cartorio, Long> {
+
+}
---
+++ src/main/java/com/projetos/cartorioos/repository/CertidaoRepository.java
@@ -0,0 +1,9 @@
+package com.projetos.cartorioos.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.projetos.cartorioos.entity.Certidao;
+
+public interface CertidaoRepository extends CrudRepository<Certidao, Long> {
+
+}
